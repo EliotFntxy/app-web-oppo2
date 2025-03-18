757a3a4ba7256dba1ec6f8b8942e4cf39d47c35a
EliotFntxy <eliotwars@hotmail.com>
2025-03-18 16:41:56 -0600

    build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
-
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
 
 pip install -r requirements.txt
-cd mi_proyecto  # Cambiar al directorio que contiene manage.py
+cd mi_proyecto  # Directorio que contiene manage.py
 python manage.py collectstatic --no-input
 python manage.py migrate
